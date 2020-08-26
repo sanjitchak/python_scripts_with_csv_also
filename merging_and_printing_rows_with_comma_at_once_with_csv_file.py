88f9f56dd3a6803a77b1dd588b9f6e2834e68d51
Sanjit Chakrabarti <sanjit@sanjitchakrabarti.com>
2020-08-26 19:19:14 +0530

    Add files via upload

---
+++ merging_and_printing_rows_with_comma_at_once_with_csv_file.py
@@ -0,0 +1,26 @@
+import csv
+
+# KEYWORD file name
+filename = "casualshirt_keyword.csv"
+
+# initializing the titles and rows list
+fields = []
+rows = []
+same = {}
+
+# reading csv file
+with open(filename, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+
+
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+    
+    #printing keyowords one by one with comma at end
+    for row in rows:
+     if same.get(row[0]) == 1 or row[0]=="" or row[0]==" ":
+         continue
+     same[row[0]] = 1
+     print(row[0],end = ',')
